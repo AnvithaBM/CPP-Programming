bdb5fbb04b18ad292f22307b85b971a58045ae36
Anvitha B M <anvitha2602@gmail.com>
2026-09-17 14:22:40 +0530

    Delete Intoduction directory

--- Intoduction/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream> //HEader file
-using namespace std; // namespace declaration
-
-int main()  // Execution begins at main
-{
-    cout<<"Hello World"<<endl; // Simple statement
-    return 0; // return statement
-}
--- Intoduction/start.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout<<"My name is Anvitha, Today I am starting C++ learning journey "<<endl;
-    cout<<"I a happy to learn something new!!"<<endl;
-}
